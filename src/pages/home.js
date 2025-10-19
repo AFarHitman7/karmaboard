e35d1e04dfcca966431d9f35926aeab07228d205
AFarHitman7 <rafanhatim@gmail.com>
2025-10-19 18:11:59 +0530

    added campus details

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,6 +6,7 @@ import TopTeam from "../components/topTeam";
 import "./styles.css";
 
 export default function Home() {
+  const [campusDetails, setCampusDetails] = useState({});
   const [monthlyStudents, setMonthlyStudents] = useState([]);
   const [students, setStudents] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -70,6 +71,31 @@ export default function Home() {
     fetchStudents();
   }, []);
 
+  useEffect(() => {
+    const fetchCampusDetails = async () => {
+      try {
+        setLoading(true);
+
+        const { data, error } = await supabase
+          .from("campus_details")
+          .select("*");
+
+        if (error) {
+          throw error;
+        }
+
+        setCampusDetails(data);
+      } catch (error) {
+        setError(error.message);
+        console.error("Error fetching campus Details:", error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCampusDetails();
+  }, []);
+
   if (loading) {
     return <p>Loading data...</p>;
   }
@@ -78,12 +104,37 @@ export default function Home() {
     return <p style={{ color: "red" }}>Error: {error}</p>;
   }
 
+  const kFormatter = new Intl.NumberFormat("en-US", {
+    notation: "compact",
+    compactDisplay: "short",
+    maximumFractionDigits: 0,
+  });
+
   return (
     <div className="page">
       <nav className="navbar">
         <div className="title">Karmayodha</div>
+
         <img className="logo" src="/mutly.png" alt="Mulearn logo" />
       </nav>
+      <div className="campus-details">
+        <div className="detailbox">
+          <p>Campus Rank</p>
+          {campusDetails[0].rank}
+        </div>
+        <div className="detailbox">
+          <p>Campus Karma</p>
+          {kFormatter.format(campusDetails[0].karma)}
+        </div>
+        <div className="detailbox">
+          <p>Active members</p>
+          {campusDetails[0].active_members}
+        </div>
+        <div className="detailbox">
+          <p>Total members</p>
+          {campusDetails[0].total_members}
+        </div>
+      </div>
       <div className="main-container">
         <div className="monthly">
           <h2 className="monthly-heading">Monthly Ranking</h2>
